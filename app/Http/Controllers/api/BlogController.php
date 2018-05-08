bf172007408eddc4762105d3bcd6e1b761b1ac24
shrouk <shrouk.passiony@gmail.com>
2018-05-08 15:21:40 +0200

    first

---
+++ app/Http/Controllers/api/BlogController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Blog;
+
+
+class BlogController extends Controller
+{
+     public function create (Request $request)
+     {
+     	$data = $request->all();
+     	// dd($data);
+     	Blog::create($data);
+        return Response()->json(array(
+                               'status'=>'success',
+                               "messages"=> array( ["text"=> "great successfully added"] )
+
+                                
+                                    ));
+
+     }
+
+
+    public function blogs()
+    {
+      $blog=Blog::first();
+      return Response()->json(array('messages'=>array(["text"=>$blog->content]) )); 
+    }
+
+
+}
